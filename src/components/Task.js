03e9f0fcb12b323b36f3c3311763aa5cd2d508d0
msko <kms89123@gmail.com>
2022-07-14 23:06:00 +0900

    <fix> change Task style

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,7 +8,7 @@ import {
   Box,
   VisuallyHidden,
 } from '@chakra-ui/react';
-import { BellIcon } from '@chakra-ui/icons';
+import { StarIcon } from '@chakra-ui/icons';
 
 export const Task = ({
   task: { id, title, state },
@@ -18,19 +18,19 @@ export const Task = ({
   ...props
 }) => (
   <Flex
-    as="li"
+    as='li'
     _notLast={{
       borderBottom: '1px',
       borderColor: 'gray.200',
     }}
     h={12}
-    bg="white"
-    alignItems="center"
+    bg='white'
+    alignItems='center'
     _hover={{
       bgGradient: 'linear(to-b,  brand.100,  brand.50)',
     }}
     aria-label={title}
-    tabIndex="0"
+    tabIndex='0'
     {...props}
   >
     <Checkbox
@@ -40,15 +40,14 @@ export const Task = ({
     >
       <VisuallyHidden>Archive</VisuallyHidden>
     </Checkbox>
-    <Box width="full" as="label">
+    <Box width='full' as='label'>
       <VisuallyHidden>Edit</VisuallyHidden>
       <Input
-        variant="unstyled"
-        flex="1 1 auto"
+        variant='unstyled'
+        flex='1 1 auto'
         color={state === 'TASK_ARCHIVED' ? 'gray.400' : 'gray.700'}
         textDecoration={state === 'TASK_ARCHIVED' ? 'line-through' : 'none'}
-        fontSize="md"
-        fontWeight="bold"
+        fontSize='sm'
         isTruncated
         value={title}
         onChange={(e) => onEditTitle(e.target.value, id)}
@@ -56,10 +55,10 @@ export const Task = ({
     </Box>
     <IconButton
       p={5}
-      flex="none"
+      flex='none'
       aria-label={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
       variant={state === 'TASK_PINNED' ? 'unpin' : 'pin'}
-      icon={<BellIcon />}
+      icon={<StarIcon />}
       onClick={() => onTogglePinTask(state, id)}
     />
   </Flex>
